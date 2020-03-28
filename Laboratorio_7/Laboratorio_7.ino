7748ee0139ff00fd81e3decde7b38b33b44101ab
HKlee057 <45294682+HKlee057@users.noreply.github.com>
2020-03-27 22:21:31 -0600

    Laboratorio finalizado
    Se finalió el código base del laboratorio. Comentarios aun son necesarios.

--- Laboratorio_7/Laboratorio_7.ino
+++ Laboratorio_7/Laboratorio_7.ino
@@ -8,51 +8,130 @@
 #include <SD.h>
 
 File myFile;
-int x = 0;
+String x = "";
+int i = 0;
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  SPI.setModule(0);
 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
- 
-  SPI.setModule(0);
-  Serial.print("Initializing SD card...");
+  Serial.print("Iniciando tarjeta SD...");
   if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
+    Serial.println("¡Inicialización fallida!");
     while (1);
   }
-  Serial.println("initialization done.");
+  Serial.println("Inicialización completada.");
   Serial.println("");
-  Serial.println("Please enter your character");
+  Serial.println("Ingrese alguno de los siguientes:");
   Serial.println("");
-  Serial.println("Vader");
+  Serial.println("-VADER");
   Serial.println("");
-  Serial.println("R2D2");
+  Serial.println("-R2D2");
   Serial.println("");
-  Serial.println("MegaMan");  
+  Serial.println("-MEGAMAN");  
+  Serial.println("");
+  Serial.println("-END (para cerrar)"); 
 }
 
 void loop() {
-  if(Serial.available()>0){
-  x = Serial.read();
-  }
-  if(x==86){
+    while (Serial.available()){
+      char character = Serial.read();
+      if (character != '\n'){
+        x.concat(character);
+      }
+    } 
+  if(x=="VADER"){
     // re-open the file for reading:
-    myFile = SD.open("Vader.txt");
+    myFile = SD.open("VADER.txt");
     if (myFile) {
-      Serial.println("Archivo:");
+      Serial.println("Desplegando a Darth Vader:");
       // read from the file until there's nothing else in it:
       while (myFile.available()) {
         Serial.write(myFile.read());
       }
       // close the file:
       myFile.close();
+      Serial.println("");
+      Serial.println("Ingrese alguno de los siguientes:");
+      Serial.println("");
+      Serial.println("-VADER");
+      Serial.println("");
+      Serial.println("-R2D2");
+      Serial.println("");
+      Serial.println("-MEGAMAN");  
+      Serial.println("");
+      Serial.println("-END (para cerrar)"); 
+      x = ""; //Limpia la variable inicial
     } 
     else {
       // if the file didn't open, print an error:
-      Serial.println("error opening test.txt");
+      Serial.println("Error desplegando el archivo.");
     }
+  }
+    if(x=="R2D2"){
+    // re-open the file for reading:
+    myFile = SD.open("R2D2.txt");
+    if (myFile) {
+      Serial.println("Desplegando a R2-D2:");
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+      Serial.println("");
+      Serial.println("Ingrese alguno de los siguientes:");
+      Serial.println("");
+      Serial.println("-VADER");
+      Serial.println("");
+      Serial.println("-R2D2");
+      Serial.println("");
+      Serial.println("-MEGAMAN");  
+      Serial.println("");
+      Serial.println("-END (para cerrar)"); 
+      x = ""; //Limpia la variable inicial
+    } 
+    else {
+      // if the file didn't open, print an error:
+      Serial.println("Error desplegando el archivo.");
+    }
+  }
+    if(x=="MEGAMAN"){
+    // re-open the file for reading:
+    myFile = SD.open("MEGAMAN.txt");
+    if (myFile) {
+      Serial.println("Desplegando a MegaMan:");
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+      Serial.println("");
+      Serial.println("Ingrese alguno de los siguientes:");
+      Serial.println("");
+      Serial.println("-VADER");
+      Serial.println("");
+      Serial.println("-R2D2");
+      Serial.println("");
+      Serial.println("-MEGAMAN");  
+      Serial.println("");
+      Serial.println("-END (para cerrar)"); 
+      x = ""; //Limpia la variable inicial
+    } 
+    else {
+      // if the file didn't open, print an error:
+      Serial.println("Error desplegando el archivo.");
+    }
+  }
+  if(x=="END"){
+      if(i<1){
+        i++;
+        Serial.println("Adiós.");
+        }
+      Serial.end();
   }
 }
